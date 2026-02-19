11542756650cafb3be533d702cd90c68600ab065
Pau Ruiz <1669357@uab.cat>
2026-02-19 20:34:46 +0100

    Añadido dominio público en PythonAnywhere

--- miniCRM/settings.py
+++ miniCRM/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(!hx#7(jbz50tdge*6y&d-+oe*wnzk%rwn-lkrz6uoarqpu80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['PauRuiz.pythonanywhere.com']
 
 
 # Application definition
