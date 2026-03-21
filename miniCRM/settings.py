d72f0e05b2681f1044b0e5838d35113ad0ba27cf
PauRuiz-TFG <1669357@uab.cat>
2026-03-21 14:45:13 +0100

    Update settings.py

--- miniCRM/settings.py
+++ miniCRM/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(!hx#7(jbz50tdge*6y&d-+oe*wnzk%rwn-lkrz6uoarqpu80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['PauRuiz.pythonanywhere.com', '127.0.0.1', 'localhost']
 
